280cc1800f3102265b8838a7201da170c11d169f
ikhwanhsn <ikhwanulhusna111@gmail.com>
2025-11-17 14:11:00 +0700

    fix: on cronjob

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,17 +17,17 @@ app.use(x402Middleware);
 app.use(express.json());
 app.use("/api/update-signals", updateSignals);
 app.use("/api/corbits-nansen", corbitsNansen);
-app.use("/api/get-crypto-price", getCryptoPrice);
+app.get("/api/get-crypto-price", getCryptoPrice);
 app.use("/btc-price", createCorbitsRoutes);
 
 app.get("/", (_, res) => res.send("Server is running 🚀"));
 
 // 🕒 Run every 5 minutes (*/5 * * * *)
 // You can adjust timing based on your needs
-// cron.schedule("*/5 * * * * *", async () => {
-//   console.log("⏰ Cron job triggered: Running signal updater");
-//   await runSignalUpdater();
-// });
+cron.schedule("*/5 * * * * *", async () => {
+  console.log("⏰ Cron job triggered: Running signal updater");
+  await runSignalUpdater();
+});
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`✅ Server running on http://0.0.0.0:${port}`);
