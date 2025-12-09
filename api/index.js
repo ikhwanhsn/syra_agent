792984c1f8d54f65da376fea6c1adde390cd5f98
ikhwanhsn <ikhwanulhusna111@gmail.com>
2025-12-09 09:45:47 +0700

    feat: add dexscreener api

--- api/index.js
+++ api/index.js
@@ -60,7 +60,7 @@ app.get("/favicon.ico", (req, res) => {
 app.use(
   rateLimit({
     windowMs: 10 * 1000, // 10 seconds
-    max: 30, // max 5 requests in 10 sec
+    max: 50, // max 10 requests in 10 sec
   })
 );
 
