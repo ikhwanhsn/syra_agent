fb9f4abce124f6d0ba355b83e86f3d6764e7b69e
ikhwanhsn <ikhwanulhusna111@gmail.com>
2026-01-01 12:06:29 +0700

    test: add ohlc binance

--- api/routes/partner/binance/ohlc.js
+++ api/routes/partner/binance/ohlc.js
@@ -1,8 +1,8 @@
-export async function getOhlc(req, res) {
+export async function getOhlc(req) {
   const { symbol = "BTCUSDT", interval = "1m" } = req.query;
   const response = await fetch(
     `https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=${interval}`
   );
   const data = await response;
-  res.json(data);
+  return data;
 }
